6057cbd8e71370e995495c435fa2f6d202b92051
rupali317 <rupali317@gmail.com>
2025-11-02 22:40:21 +0800

    [JS Refactoring] Remove duplicate code, create common function

--- main.js
+++ main.js
@@ -10,6 +10,12 @@ const activities = {
   selfcare: 5,
 };
 
+function updateAllActivities(activity, current, previous) {
+  document.getElementById(`js-${activity}-current-period`).innerText = current;
+  document.getElementById(`js-${activity}-previous-period`).innerText =
+    previous;
+}
+
 function getDataBasedOnTimePeriod(TIME_PERIOD) {
   fetch("data.json")
     .then((response) => response.json())
@@ -21,33 +27,14 @@ function getDataBasedOnTimePeriod(TIME_PERIOD) {
       });
 
       if (TIME_PERIOD === 0) {
-        // Daily current
-        document.getElementById("js-work-current-period").innerText =
-          data[0].timeframes.daily.current;
-        document.getElementById("js-play-current-period").innerText =
-          data[1].timeframes.daily.current;
-        document.getElementById("js-study-current-period").innerText =
-          data[2].timeframes.daily.current;
-        document.getElementById("js-exercise-current-period").innerText =
-          data[3].timeframes.daily.current;
-        document.getElementById("js-social-current-period").innerText =
-          data[4].timeframes.daily.current;
-        document.getElementById("js-selfcare-current-period").innerText =
-          data[5].timeframes.daily.current;
-
-        // Daily previous
-        document.getElementById("js-work-previous-period").innerText =
-          data[0].timeframes.daily.previous;
-        document.getElementById("js-play-previous-period").innerText =
-          data[1].timeframes.daily.previous;
-        document.getElementById("js-study-previous-period").innerText =
-          data[2].timeframes.daily.previous;
-        document.getElementById("js-exercise-previous-period").innerText =
-          data[3].timeframes.daily.previous;
-        document.getElementById("js-social-previous-period").innerText =
-          data[4].timeframes.daily.previous;
-        document.getElementById("js-selfcare-previous-period").innerText =
-          data[5].timeframes.daily.previous;
+        // Daily timeframe
+        Object.keys(activities).forEach((activity, index) => {
+          updateAllActivities(
+            activity,
+            data[index].timeframes.daily.current,
+            data[index].timeframes.daily.previous
+          );
+        });
 
         // Mark the current button as active
         document.getElementById("js-button-daily").classList.add("active");
@@ -56,33 +43,14 @@ function getDataBasedOnTimePeriod(TIME_PERIOD) {
         document.getElementById("js-button-weekly").classList.remove("active");
         document.getElementById("js-button-monthly").classList.remove("active");
       } else if (TIME_PERIOD === 1) {
-        // Weekly current
-        document.getElementById("js-work-current-period").innerText =
-          data[0].timeframes.weekly.current;
-        document.getElementById("js-play-current-period").innerText =
-          data[1].timeframes.weekly.current;
-        document.getElementById("js-study-current-period").innerText =
-          data[2].timeframes.weekly.current;
-        document.getElementById("js-exercise-current-period").innerText =
-          data[3].timeframes.weekly.current;
-        document.getElementById("js-social-current-period").innerText =
-          data[4].timeframes.weekly.current;
-        document.getElementById("js-selfcare-current-period").innerText =
-          data[5].timeframes.weekly.current;
-
-        // Weekly previous
-        document.getElementById("js-work-previous-period").innerText =
-          data[0].timeframes.weekly.previous;
-        document.getElementById("js-play-previous-period").innerText =
-          data[1].timeframes.weekly.previous;
-        document.getElementById("js-study-previous-period").innerText =
-          data[2].timeframes.weekly.previous;
-        document.getElementById("js-exercise-previous-period").innerText =
-          data[3].timeframes.weekly.previous;
-        document.getElementById("js-social-previous-period").innerText =
-          data[4].timeframes.weekly.previous;
-        document.getElementById("js-selfcare-previous-period").innerText =
-          data[5].timeframes.weekly.previous;
+        // Weekly timeframe
+        Object.keys(activities).forEach((activity, index) => {
+          updateAllActivities(
+            activity,
+            data[index].timeframes.weekly.current,
+            data[index].timeframes.weekly.previous
+          );
+        });
 
         // Mark the current button as active
         document.getElementById("js-button-weekly").classList.add("active");
@@ -91,33 +59,14 @@ function getDataBasedOnTimePeriod(TIME_PERIOD) {
         document.getElementById("js-button-daily").classList.remove("active");
         document.getElementById("js-button-monthly").classList.remove("active");
       } else if (TIME_PERIOD === 2) {
-        // Monthly current
-        document.getElementById("js-work-current-period").innerText =
-          data[0].timeframes.monthly.current;
-        document.getElementById("js-play-current-period").innerText =
-          data[1].timeframes.monthly.current;
-        document.getElementById("js-study-current-period").innerText =
-          data[2].timeframes.monthly.current;
-        document.getElementById("js-exercise-current-period").innerText =
-          data[3].timeframes.monthly.current;
-        document.getElementById("js-social-current-period").innerText =
-          data[4].timeframes.monthly.current;
-        document.getElementById("js-selfcare-current-period").innerText =
-          data[5].timeframes.monthly.current;
-
-        // Monthly previous
-        document.getElementById("js-work-previous-period").innerText =
-          data[0].timeframes.monthly.previous;
-        document.getElementById("js-play-previous-period").innerText =
-          data[1].timeframes.monthly.previous;
-        document.getElementById("js-study-previous-period").innerText =
-          data[2].timeframes.monthly.previous;
-        document.getElementById("js-exercise-previous-period").innerText =
-          data[3].timeframes.monthly.previous;
-        document.getElementById("js-social-previous-period").innerText =
-          data[4].timeframes.monthly.previous;
-        document.getElementById("js-selfcare-previous-period").innerText =
-          data[5].timeframes.monthly.previous;
+        // Monthly timeframe
+        Object.keys(activities).forEach((activity, index) => {
+          updateAllActivities(
+            activity,
+            data[index].timeframes.monthly.current,
+            data[index].timeframes.monthly.previous
+          );
+        });
 
         // Mark the current button as active
         document.getElementById("js-button-monthly").classList.add("active");
