a4db8a4edcc4e442e023a6132492295d90498e2b
rupali317 <rupali317@gmail.com>
2025-11-02 19:43:28 +0800

    Setting up ids in HTMLs to help in JS logic, setup basic JS function

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-function LoadDefaultData() {
-  getDataBasedOnTimePeriod(0);
-}
+// Load default data
+getDataBasedOnTimePeriod(0);
 
 function getDataBasedOnTimePeriod(TIME_PERIOD) {
-  if (TIME_PERIOD === 0) {
-    console.log("day has been selected");
-  } else if (TIME_PERIOD === 1) {
-    console.log("week has been selected");
-  } else if (TIME_PERIOD === 2) {
-    console.log("month has been selected");
-  }
+  // read data from json file
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data);
+      // Set titles
+      document.getElementById("js-work-heading").innerText = "Work";
+      document.getElementById("js-play-heading").innerText = "Play";
+      document.getElementById("js-study-heading").innerText = "Study";
+      document.getElementById("js-exercise-heading").innerText = "Exercise";
+      document.getElementById("js-social-heading").innerText = "Social";
+      document.getElementById("js-selfcare-heading").innerText = "Self Care";
+      if (TIME_PERIOD === 0) {
+        // 2. read data for daily
+        // 3. mark the current button as active
+        // 4. remove the active class from the other button
+      } else if (TIME_PERIOD === 1) {
+        // 2. read data for weekly
+        // 3. mark the current button as active
+        // 4. remove the active class from the other button
+      } else if (TIME_PERIOD === 2) {
+        // 2. read data for monthly
+        // 3. mark the current button as active
+        // 4. remove the active class from the other button
+      }
+    });
 }
-
-LoadDefaultData();
