3541f89d0cd23e72e25aa7108028ebe1c9f2c035
rupali317 <rupali317@gmail.com>
2025-11-03 16:06:20 +0800

    [JS Refactoring] DRY

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // Load default data
 getDataBasedOnTimePeriod(0);
 
-const activities = {
+const activitiesMap = {
   work: 0,
   play: 1,
   study: 2,
@@ -10,10 +10,20 @@ const activities = {
   selfcare: 5,
 };
 
+const timeFrameMap = ["daily", "weekly", "monthly"];
+
 function updateAllActivities(activity, current, previous) {
   document.getElementById(`js-${activity}-current-period`).innerText = current;
   document.getElementById(`js-${activity}-previous-period`).innerText =
     previous;
+}
+
+function setActiveTimeFrame(activePeriod) {
+  const timePeriodList = document.querySelectorAll("[id*=js-button]");
+  timePeriodList.forEach((timePeriod) => {
+    timePeriod.classList.remove("active");
+  });
+  document.getElementById(`js-button-${activePeriod}`).classList.add("active");
 }
 
 function getDataBasedOnTimePeriod(TIME_PERIOD) {
@@ -21,59 +31,41 @@ function getDataBasedOnTimePeriod(TIME_PERIOD) {
     .then((response) => response.json())
     .then((data) => {
       // Set titles
-      Object.keys(activities).forEach((activity, index) => {
+      Object.keys(activitiesMap).forEach((activity, index) => {
         document.getElementById(`js-${activity}-heading`).innerText =
           data[index].title;
       });
-
+      // Daily timeframe
       if (TIME_PERIOD === 0) {
-        // Daily timeframe
-        Object.keys(activities).forEach((activity, index) => {
+        Object.keys(activitiesMap).forEach((activity, index) => {
           updateAllActivities(
             activity,
             data[index].timeframes.daily.current,
             data[index].timeframes.daily.previous
           );
         });
-
-        // Mark the current button as active
-        document.getElementById("js-button-daily").classList.add("active");
-
-        // Remove the active class from the other button
-        document.getElementById("js-button-weekly").classList.remove("active");
-        document.getElementById("js-button-monthly").classList.remove("active");
-      } else if (TIME_PERIOD === 1) {
-        // Weekly timeframe
-        Object.keys(activities).forEach((activity, index) => {
+        setActiveTimeFrame(timeFrameMap[0]);
+      } // Weekly timeframe
+      else if (TIME_PERIOD === 1) {
+        Object.keys(activitiesMap).forEach((activity, index) => {
           updateAllActivities(
             activity,
             data[index].timeframes.weekly.current,
             data[index].timeframes.weekly.previous
           );
         });
-
-        // Mark the current button as active
-        document.getElementById("js-button-weekly").classList.add("active");
-
-        // Remove the active class from the other button
-        document.getElementById("js-button-daily").classList.remove("active");
-        document.getElementById("js-button-monthly").classList.remove("active");
-      } else if (TIME_PERIOD === 2) {
-        // Monthly timeframe
-        Object.keys(activities).forEach((activity, index) => {
+        setActiveTimeFrame(timeFrameMap[1]);
+      } // Monthly timeframe
+      else if (TIME_PERIOD === 2) {
+        Object.keys(activitiesMap).forEach((activity, index) => {
           updateAllActivities(
             activity,
             data[index].timeframes.monthly.current,
             data[index].timeframes.monthly.previous
           );
         });
-
-        // Mark the current button as active
-        document.getElementById("js-button-monthly").classList.add("active");
-
-        // Remove the active class from the other button
-        document.getElementById("js-button-daily").classList.remove("active");
-        document.getElementById("js-button-weekly").classList.remove("active");
+        setActiveTimeFrame(timeFrameMap[2]);
       }
-    });
+    })
+    .catch((error) => console.error("Error loading data: " + error));
 }
